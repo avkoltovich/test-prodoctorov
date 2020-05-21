450cb390fcd8ddd9e578c755efea82d3fab5d5f0
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-05-21 20:34:54 +0300

    refactor: дорабатывает функцию render

--- js/utils/render.js
+++ js/utils/render.js
@@ -1,6 +1,22 @@
-const render = (container, component) => {
+const InsertionPosition = {
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`,
+  BEFOREEND: `beforeend`
+};
+
+const render = (container, component, position) => {
   if (container instanceof AbstractComponent) {
     container = container.getElement();
   }
-  container.after(component.getElement());
+  switch (position) {
+    case InsertionPosition.AFTERBEGIN:
+      container.prepend(component.getElement());
+      break;
+    case InsertionPosition.AFTEREND:
+      container.after(component.getElement());
+      break;
+    case InsertionPosition.BEFOREEND:
+      container.append(component.getElement());
+      break;
+  }
 };
